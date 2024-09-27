4105525d010db0a2b4889037f08116c1f19004db
William Castillo <william.castillo.jr@outlook.com>
2024-09-27 14:57:44 -0700

    Experimented with faker package

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import lodash from 'lodash';
 import chalk from 'chalk';
+import { faker } from '@faker-js/faker'
 
-const array = [1, 2, 3, 4, 4, 6, 6, 9];
+const array = [1, 2, 3, 4, 4, 6, 6, ];
 const uniqueNums = lodash.uniq(array);
+console.log(chalk.red(uniqueNums));
 console.log(chalk.yellow(uniqueNums));
 
 console.log(chalk.blueBright("Hello, with chalk!"));
+
+const paragraph = faker.lorem.paragraph();
+console.log(paragraph);
+
+const location = faker.location.streetAddress();
+console.log(`I am located at ${location}`);
